151254842ec2226c12b02021cf070d3534267fc0
cahilljpaul <cahilljpaul@googlemail.com>
2025-03-18 08:03:32 +0000

    new

--- index.py
+++ index.py
@@ -10,3 +10,11 @@ def show_tasks():
         print("Tasks:")
         for index, task in enumerate(tasks, start=1):
             print(f"{index}. {task}")
+show_tasks()
+
+#Add a new task 
+def add_task():
+    """Add a new task"""
+    task = input("/ Enter the task: ")
+    tasks.append(task)
+    print(f"Task '{task}' added")'''
